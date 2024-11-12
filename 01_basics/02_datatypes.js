7273d82af188dd90c71e5176b1d2b53bc01e91e6
VIKAS7216 <166967375+VIKAS7216@users.noreply.github.com>
2024-11-12 03:39:37 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+'use strict'; // treats all code as JS newer version
+
+console.log(3+3)
+console.log("Vikas")
+// readability should be high
+
+let age=3
+let name='vikas'
+
+//number =>2 to the power 53
+// bigint
+/* string
+boolean=> true/false
+undefined=> value is not given
+null=> standalone output
+object
+*/
+console.log( typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+// null is an object data type
+
+
