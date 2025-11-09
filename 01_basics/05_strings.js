4abc59a00e1446a967b797bcb1c5cf3fb13e15f8
VIKAS7216 <166967375+VIKAS7216@users.noreply.github.com>
2025-11-09 16:30:59 +0000

    revised

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,7 +2,7 @@ const name = "hitesh"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
-
+// String Interpolation
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('hitesh-hc-com')
